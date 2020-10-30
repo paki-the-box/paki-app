c547a7040063531312e0c10da72885270831e412
Niklas Merz <niklasmerz@linux.com>
2020-10-30 22:21:44 +0100

    Some boxes page UI

--- src/app/boxes/boxes.page.ts
+++ src/app/boxes/boxes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemReorderEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-boxes',
@@ -10,6 +11,17 @@ export class BoxesPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  doReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
   }
 
 }
