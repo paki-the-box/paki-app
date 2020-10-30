a2621634e578b3a5d0d68549bb2b940e09b76fd0
Julian Feinauer <j.feinauer@pragmaticminds.de>
2020-10-30 21:03:18 +0100

    Oauth Login works.

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,29 +7,33 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  public token: string;
+  public token?: string;
+  public isLogin: boolean = false
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.activatedRoute.paramMap.subscribe((params) => {
-      console.log("Hallo")
-      console.log(params)
-      this.token = params.get('access_token');
-      console.debug(this.token);
+    this.activatedRoute.fragment.subscribe((data) => {
+      let fragments = data.split("&");
+      console.log(fragments)
+      for (let fragment of fragments) {
+        console.log(fragment)
+        let strings = fragment.split("=");
+        console.log(strings)
+        if (strings[0] == "access_token") {
+          console.log("Access Token is set...")
+          let access_token = strings[1];
+          this.token = access_token
+          this.isLogin = true;
+          return;
+        }
+      }
     });
   }
 
   login() {
     // window.open("https://auth.demo.pragmaticindustries.de/auth/realms/packi/protocol/openid-connect/auth?client_id=packi_app&redirect_uri=" + encodeURIComponent("https://paki.pragmaticminds.de/auth-callback") + "&response_type=token&scope=email", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
     window.open("https://auth.demo.pragmaticindustries.de/auth/realms/packi/protocol/openid-connect/auth?client_id=packi_app&redirect_uri=" + encodeURIComponent("http://localhost:4200/login") + "&response_type=token&scope=email", "_self", "location=no,clearsessioncache=yes,clearcache=yes");
-  }
-
-  public oauthLogin() {
-    return new Promise(function(resolve, reject) {
-      // https://auth.demo.pragmaticindustries.de/auth/realms/packi/protocol/openid-connect/auth?response_type=token&state=&client_id=packi_app&scope=email&redirect_uri=https%3A%2F%2Foauth.pstmn.io%2Fv1%2Fcallback
-      window.open("https://auth.demo.pragmaticindustries.de/auth/realms/packi/protocol/openid-connect/auth?client_id=packi_app&redirect_uri=" + encodeURIComponent("https://paki.pragmaticminds.de/auth-callback") + "&response_type=token&scope=email", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
-    });
   }
 
 }
