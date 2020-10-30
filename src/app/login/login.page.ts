895205c78240a18ac5257bf147a16c4f0d330c70
Julian Feinauer <j.feinauer@pragmaticminds.de>
2020-10-30 19:18:37 +0100

    Added OAUTH

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -13,13 +13,23 @@ export class LoginPage implements OnInit {
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe((params) => {
-      this.token = params.get('token');
+      console.log("Hallo")
+      console.log(params)
+      this.token = params.get('access_token');
       console.debug(this.token);
     });
   }
 
   login() {
-    window.open('https://niklas.merz.dev/login-test');
+    // window.open("https://auth.demo.pragmaticindustries.de/auth/realms/packi/protocol/openid-connect/auth?client_id=packi_app&redirect_uri=" + encodeURIComponent("https://paki.pragmaticminds.de/auth-callback") + "&response_type=token&scope=email", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+    window.open("https://auth.demo.pragmaticindustries.de/auth/realms/packi/protocol/openid-connect/auth?client_id=packi_app&redirect_uri=" + encodeURIComponent("http://localhost:4200/login") + "&response_type=token&scope=email", "_self", "location=no,clearsessioncache=yes,clearcache=yes");
+  }
+
+  public oauthLogin() {
+    return new Promise(function(resolve, reject) {
+      // https://auth.demo.pragmaticindustries.de/auth/realms/packi/protocol/openid-connect/auth?response_type=token&state=&client_id=packi_app&scope=email&redirect_uri=https%3A%2F%2Foauth.pstmn.io%2Fv1%2Fcallback
+      window.open("https://auth.demo.pragmaticindustries.de/auth/realms/packi/protocol/openid-connect/auth?client_id=packi_app&redirect_uri=" + encodeURIComponent("https://paki.pragmaticminds.de/auth-callback") + "&response_type=token&scope=email", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+    });
   }
 
 }
