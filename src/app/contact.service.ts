60064ab5e1a3b457596d874784ee4ddd88c7fd0b
Niklas Merz <niklasmerz@linux.com>
2020-10-31 08:54:08 +0100

    Persons

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -9,9 +9,12 @@ export class ContactService {
 
     private contacts = [
         new Contact(1, 'Julian Feinauer', 'https://ca.slack-edge.com/T01BWJSLH9V-U01DL19HR6H-g799b8ba68f5-512', [1, 2]),
-        new Contact(2, 'Niklas Merz', 'https://ca.slack-edge.com/T01BWJSLH9V-U01DGBU5TE2-9c36519a20c7-512'),
-        new Contact(3, 'Björn Höper'),
-        new Contact(4, 'Simon Mayer', null),
+        new Contact(2, 'Niklas Merz', 'https://ca.slack-edge.com/T01BWJSLH9V-U01DGBU5TE2-9c36519a20c7-512', [1]),
+        new Contact(3, 'Björn Höper', undefined, [1, 2]),
+        new Contact(4, 'Simon Mayer'),
+        new Contact(5, 'Andreas Schulz'),
+        new Contact(6, 'Moritz Bernhardt'),
+        new Contact(7, 'Tim Mitsch'),
     ];
 
     constructor() {
