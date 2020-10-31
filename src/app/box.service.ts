75267e96423eb54a7d2d68dcf3e5c70694d1f2fd
Julian Feinauer <j.feinauer@pragmaticminds.de>
2020-10-31 03:24:31 +0100

    Removed unnecessary image from Box.

--- src/app/box.service.ts
+++ src/app/box.service.ts
@@ -7,8 +7,8 @@ import {Box} from "./box";
 export class BoxService {
 
   private boxes = [
-      new Box(1, "Um die Ecke", 'https://ca.slack-edge.com/T01BWJSLH9V-U01DL19HR6H-g799b8ba68f5-512', '53.5477251', '9.9438548'),
-      new Box(2, "Um die andere Ecke", undefined, '53.540215', '9.9980949'),
+      new Box(1, "Um die Ecke", '53.5477251', '9.9438548'),
+      new Box(2, "Um die andere Ecke", '53.540215', '9.9980949'),
   ]
 
   constructor() { }
