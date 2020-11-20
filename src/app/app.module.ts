7244ac6a7e42d68d0e573e8706bc195c410b3a8b
Julian Feinauer <j.feinauer@pragmaticminds.de>
2020-11-20 16:02:13 +0100

    Added OAuth support.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
+import {OAuthModule} from 'angular-oauth2-oidc';
 
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
@@ -8,7 +9,6 @@ import {StatusBar} from '@ionic-native/status-bar/ngx';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
-import {ComponentsModule} from './components/components.module';
 import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
@@ -18,7 +18,8 @@ import {HttpClientModule} from "@angular/common/http";
         BrowserModule,
         IonicModule.forRoot(),
         AppRoutingModule,
-        HttpClientModule
+        HttpClientModule,
+        OAuthModule.forRoot()
     ],
     providers: [
         StatusBar,
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {AuthConfig, OAuthService} from 'angular-oauth2-oidc';
+import {addWarning} from "@angular-devkit/build-angular/src/utils/webpack-diagnostics";
+
+export const authCodeFlowConfig: AuthConfig = {
+  // Url of the Identity Provider
+  issuer: 'https://idsvr4.azurewebsites.net',
+
+  // URL of the SPA to redirect the user to after login
+  redirectUri: window.location.origin + '/index.html',
+
+  // The SPA's id. The SPA is registerd with this id at the auth-server
+  // clientId: 'server.code',
+  clientId: 'spa',
+
+  // Just needed if your auth server demands a secret. In general, this
+  // is a sign that the auth server is not configured with SPAs in mind
+  // and it might not enforce further best practices vital for security
+  // such applications.
+  // dummyClientSecret: 'secret',
+
+  responseType: 'code',
+
+  // set the scope for the permissions the client should request
+  // The first four are defined by OIDC.
+  // Important: Request offline_access to get a refresh token
+  // The api scope is a usecase specific one
+  scope: 'openid profile email offline_access api',
+
+  showDebugInformation: true,
+};
 
 @Component({
   selector: 'app-login',
@@ -10,7 +40,7 @@ export class LoginPage implements OnInit {
   public token?: string;
   public isLogin: boolean = false
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private oauthService: OAuthService) { }
 
   ngOnInit() {
     this.activatedRoute.fragment.subscribe((data) => {
@@ -33,7 +63,13 @@ export class LoginPage implements OnInit {
 
   login() {
     // window.open("https://auth.demo.pragmaticindustries.de/auth/realms/packi/protocol/openid-connect/auth?client_id=packi_app&redirect_uri=" + encodeURIComponent("https://paki.pragmaticminds.de/auth-callback") + "&response_type=token&scope=email", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
-    window.open("https://auth.demo.pragmaticindustries.de/auth/realms/packi/protocol/openid-connect/auth?client_id=packi_app&redirect_uri=" + encodeURIComponent("http://localhost:4200/login") + "&response_type=token&scope=email", "_self", "location=no,clearsessioncache=yes,clearcache=yes");
+    // window.open("https://auth.demo.pragmaticindustries.de/auth/realms/packi/protocol/openid-connect/auth?client_id=packi_app&redirect_uri=" + encodeURIComponent("http://localhost:4200/login") + "&response_type=token&scope=email", "_self", "location=no,clearsessioncache=yes,clearcache=yes");
+    console.log("Configure")
+    this.oauthService.configure(authCodeFlowConfig)
+    console.log("Try Login")
+    this.oauthService.loadDiscoveryDocumentAndTryLogin().then(() => {
+      this.oauthService.initImplicitFlow()
+    })
   }
 
 }
